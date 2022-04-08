e275b051fe4ca6bf0d2741020514961ba4f23cc6
0tohik0 <71322445+0tohik0@users.noreply.github.com>
2022-04-08 14:16:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,23 @@
-"use strict";
+'use strict';
 
-let lang = prompt("ru", "en");
+let lang = prompt('Выберите язык', 'ru');
 
-
-if (lang == "ru") {
-    console.log("Понедельник" + " " + "Вторник" + " " + "Среда" + " " + "Четверг" + " " + "Пятница" + " " + "Суббота" + " " + "Воскресенье");
-} else if (lang == "en") {
-    console.log("Monday" + " " + "Tuesday" + " " + "Wednesday" + " " + "Thursday" + " " + "Friday" + " " + "Saturday" + " " + "Sunday");
+if (lang === 'ru') {
+    console.log('Понедельник' + ' ' + 'Вторник' + ' ' + 'Среда' + ' ' + 'Четверг' + ' ' + 'Пятница' + ' ' + 'Суббота' + ' ' + 'Воскресенье');
+} else if (lang === 'en') {
+    console.log('Monday' + ' ' + 'Tuesday' + ' ' + 'Wednesday' + ' ' + 'Thursday' + ' ' + 'Friday' + ' ' + 'Saturday' + ' ' + 'Sunday');
 } else {
-    console.log("Ошибка");
+    console.log('Выберите ru или en');
 };
-
 
 switch (lang) {
-    case "ru":
-        console.log("Понедельник" + " " + "Вторник" + " " + "Среда" + " " + "Четверг" + " " + "Пятница" + " " + "Суббота" + " " + "Воскресенье");
-        break;
-
-    case "en":
-        console.log("Monday" + " " + "Tuesday" + " " + "Wednesday" + " " + "Thursday" + " " + "Friday" + " " + "Saturday" + " " + "Sunday");
-        break;
-
+    case 'ru': 
+        console.log('Понедельник' + ' ' + 'Вторник' + ' ' + 'Среда' + ' ' + 'Четверг' + ' ' + 'Пятница' + ' ' + 'Суббота' + ' ' + 'Воскресенье');
+        break
+    case 'en': 
+        console.log('Monday' + ' ' + 'Tuesday' + ' ' + 'Wednesday' + ' ' + 'Thursday' + ' ' + 'Friday' + ' ' + 'Saturday' + ' ' + 'Sunday');
+        break
     default:
-        console.log("Ошибка");
-        break;
-};
-
-
-const language = {
-    ru: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
-    en: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-};
-console.log(language[lang]);
-
-
-let namePerson = prompt("Введите имя");
-let post = "Положение";
-
-namePerson === "Артем" ? (
-    post = "директор",
-    console.log(namePerson + ': ' + post)
-) : (
-    namePerson === "Александр" ? (
-        post = "преподаватель",
-        console.log(namePerson + ': ' + post)
-    ) : (
-        post = "студент",
-        console.log(namePerson + ': ' + post)
-    )
-);
+        console.log('Выберите ru или en');
+        break
+}
