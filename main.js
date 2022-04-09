2de3744cf9587d90a9b2cbfaa80111570aa01b97
0tohik0 <71322445+0tohik0@users.noreply.github.com>
2022-04-09 11:20:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-1
+'use strict';
+
+let test = prompt('Введите аргумент', 'name');
+
+function estimationArgumens (e) {
+    if (typeof e !== 'string') {
+        alert('Вы ввели не строку');
+    } else if (typeof e === 'string' && e.length < 30) {
+        console.log('Введена строка без пробелов' + ': ' + e.trim());
+    } else if (typeof e === 'string' && e.length > 30) {
+        console.log('Строка больше 30 символов' + ': ' + e.trim().substring(0, 30) + '...');
+    }
+};
+
+estimationArgumens(test);
+
